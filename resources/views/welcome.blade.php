294b5c00573c0e8ef17bde93e33a7fa254622eb6
Git Patrick <patrick.wan@kinchotech.com>
2023-07-11 20:39:29 +0800

    feat: add pwa function

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,6 +10,7 @@
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         @vite(['resources/js/app.js'])
+        @laravelPWA
     </head>
     <body>
         <div id="app">

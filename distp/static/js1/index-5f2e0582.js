548e7de945ae6ea6bf563870ac0a8f851fa78b6c
chinyeep123 <patrickwanchinyeep@gmail.com>
2024-03-06 10:26:59 +0800

    fix: remove all distp file

--- distp/static/js1/index-5f2e0582.js
+++
@@ -1,2 +0,0 @@
-import{a8 as a,ad as n}from"./__vendor-1966d9c6.js";import{z as u,D as p}from"./_@vue-37f9eba6.js";const i=u({setup(){const e=a(),o=n();return p(()=>{const{params:t,query:r}=e,{path:s}=t;o.replace({path:`/${s}`,query:r})}),()=>"<div> </div>"}});export{i as default};
-//# sourceMappingURL=index-5f2e0582.js.map
